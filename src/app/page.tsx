4212e9e80e46557586c75b28bc961146c8f8368f
RobEriks <Rob.Eriksson@outlook.com>
2024-12-25 12:18:41 +0100

    Part 2 - Fetch/api part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const WeatherApp: React.FC = () => {
   const [weather, setWeather] = useState<any>(null);
@@ -8,8 +9,45 @@ const WeatherApp: React.FC = () => {
   const [loeading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          const apiUrl = `https://weather-proxy.freecodecamp.rocks/api/current?lat=${latitude}&lon=${longitude}`;
+          try {
+            const response = await axios.get(apiUrl);
+            setWeather(response.data);
+            setLoading(false);
+          } catch (err) {
+            setError("Failed to get weather");
+            setLoading(false);
+          }
+        },
+        () => setError("Location denied")
+      );
+    } else {
+      setError("Geolocation is not supported");
+    }
+  }, []);
+  
+  
+
+
+
+
+
+
   return <div>WeatherApp!</div>
 
+
+
+
+
 }
+
+
 
 export default WeatherApp;
